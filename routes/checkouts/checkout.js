1b86229d6e5ab09db81709aa30c08a309497a3ad
James W Moody <jamesmoodyradio@gmail.com>
2017-09-21 18:07:03 -0500

    Finish transaction logic for Drop-In UI

--- routes/checkouts/checkout.js
+++ routes/checkouts/checkout.js
@@ -1,29 +1,74 @@
-var express = require('express');
-var router = express.Router();
-var braintree = require('braintree');
-var dotenv = require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const braintree = require('braintree');
+const dotenv = require('dotenv').config();
 
 router.post('/', function(req, res, next) {
-  var gateway = braintree.connect({
+  const gateway = braintree.connect({
     environment: braintree.Environment.Sandbox,
     merchantId: process.env.BT_MERCHANT_ID,
     publicKey: process.env.BT_PUBLIC_KEY,
     privateKey: process.env.BT_PRIVATE_KEY
   });
 
-  var nonceFromTheClient = req.body.paymentMethodNonce;
-  var newTransaction = gateway.transaction.sale({
-    amount: '10.00',
-    paymentMethodNonce: nonceFromTheClient,
+  // Payment Variables
+  const nonce = req.body.nonce;
+  const amount = req.body.amount;
+
+  // Customer Variables
+  const email = req.body.email;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const streetAddress = req.body.streetAddress;
+  const city = req.body.city;
+  const state = req.body.state;
+  const country = req.body.country;
+
+  // Transaction Option Variables
+  const submitForSettlement = req.body.submitForSettlement;
+  const vault = req.body.vault;
+  const skipAtf = req.body.skipAtf;
+  const skipAvs = req.body.skipAvs;
+  const skipCvv = req.body.skipCvv;
+
+  const newTransaction = gateway.transaction.sale({
+    amount: amount,
+    paymentMethodNonce: nonce,
+    customer: {
+      firstName: firstName,
+      lastName: lastName,
+      email: email
+    },
+    billing: {
+      firstName: firstName,
+      lastName: lastName,
+      streetAddress: streetAddress,
+      locality: city,
+      region: state,
+      countryCodeAlpha2: country
+    },
     options: {
-      submitForSettlement: true
+      submitForSettlement: true,
+      storeInVaultOnSuccess: vault,
+      skipAdvancedFraudChecking: skipAtf,
+      skipAvs: skipAvs,
+      skipCvv: skipCvv
     }
-  }, function(error, result) {
-      if (result) {
-        res.send(result);
-      } else {
-        res.status(500).send(error);
-      }
+  }, function (err, result) {
+    if (result.success || result.transaction) {
+      res.render('show', {
+        transaction: result.transaction,
+        result: JSON.stringify(result, null, '    '),
+        title: 'Braintree Node.js Integration'
+      });
+    } else {
+      transactionErrors = result.errors.deepErrors();
+      console.log(transactionErrors);
+      res.render('index', {
+        errors: transactionErrors, 
+        title: 'Braintree Node.js Integration'
+      });
+    }
   });
 });
 
